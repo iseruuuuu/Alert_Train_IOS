4a314108e9cea5cac185e2192fe998e5e8b870e9
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-08-05 22:43:20 +0900

    add: 画面の作成

--- Alert_Train_App/ViewController.swift
+++ Alert_Train_App/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var TimerPicker: UIDatePicker!
+    @IBOutlet weak var SetButton: UIButton!
 
+    private var tempTime: String = "00:00:00"
+    private var setTime : String = "00:00:00"
+    var timer: Timer!
+    var alartTime = Date()
+    private var onAlartTime: [String] = []
+    var stop = false
+    //var notification = Notification()
+    var app = AppDelegate()
+    
+    var request:UNNotificationRequest!
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    super.viewDidLoad()
+    //datePickerの表示の設定
+    //    datePicker.datePickerMode = .time
+    //  datePicker.setDate(Date(), animated: false)
+    
+
+}
+
+override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+}
+    
+    
+    @IBAction func datePicker(_ sender: Any) {
+        
     }
-
-
+    
+    
+    @IBAction func setButton(_ sender: Any) {
+        
+    }
 }
 
